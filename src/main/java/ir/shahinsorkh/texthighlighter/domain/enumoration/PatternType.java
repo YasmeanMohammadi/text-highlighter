99a45ba465db87626561a7c545e496a26965dcd7
yasamean mohammadi <mohammadi@sohagroup.org>
2021-01-25 19:16:12 +0330

    database configuration was added + some changes on branch THL_4

---
+++ src/main/java/ir/shahinsorkh/texthighlighter/domain/enumoration/PatternType.java
@@ -0,0 +1,8 @@
+package ir.shahinsorkh.texthighlighter.domain.enumoration;
+
+public enum PatternType {
+    TERM,
+    NUMBER,
+    MIX,
+    HELLO
+}
--- src/main/java/ir/shahinsorkh/texthighlighter/service/impl/HighLightServiceImpl.java
+++ src/main/java/ir/shahinsorkh/texthighlighter/service/impl/HighLightServiceImpl.java
@@ -5,17 +5,19 @@ import ir.shahinsorkh.texthighlighter.service.dto.HighLightRequestDTO;
 import ir.shahinsorkh.texthighlighter.service.dto.HighLightResponseDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @Service
 public class HighLightServiceImpl implements HighLightService {
+
+    @Value("${splitString.regex}")
+    private String splitRegex;
 
     private final Logger log = LoggerFactory.getLogger(HighLightServiceImpl.class);
 
@@ -23,7 +25,7 @@ public class HighLightServiceImpl implements HighLightService {
     public HighLightResponseDTO findSingleTerm(HighLightRequestDTO highLightRequestDTO) {
         log.debug("request to find term : [{}] ", highLightRequestDTO.getTerm());
         HighLightResponseDTO highLightResponseDTO = new HighLightResponseDTO();
-        String [] source = highLightRequestDTO.getSource().split(" ");
+        String [] source = highLightRequestDTO.getSource().split(splitRegex);
         List<String> result = Arrays.stream(source).filter(s -> s.contains(highLightRequestDTO.getTerm())).collect(Collectors.toList());
         highLightResponseDTO.setWords(result);
         log.debug("response to find term [{}] is [{}] ", highLightRequestDTO.getTerm(), highLightResponseDTO);
@@ -34,7 +36,7 @@ public class HighLightServiceImpl implements HighLightService {
     public HighLightResponseDTO findByRegEx(HighLightRequestDTO highLightRequestDTO) {
         log.debug("request to find regular expression : [{}] ", highLightRequestDTO.getPattern());
         HighLightResponseDTO highLightResponseDTO = new HighLightResponseDTO();
-        String [] strings = highLightRequestDTO.getSource().split(" ");
+        String [] strings = highLightRequestDTO.getSource().split(splitRegex);
         List<String> result = Arrays.stream(strings).filter(s -> Pattern.compile(highLightRequestDTO.getPattern()).matcher(s).find()).collect(Collectors.toList());
         highLightResponseDTO.setWords(result);
         log.debug("response to find by regular expression [{}] is [{}] ", highLightRequestDTO.getPattern(), highLightResponseDTO);
